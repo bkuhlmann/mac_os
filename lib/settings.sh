be396b9581a843a59f655350c6e9f6ead37a24d2
Brooke Kuhlmann <brooke@alchemists.io>
2018-09-25 09:53:08 -0600

    Updated to Java SE Development Kit 10.
    Provides a link to the latest JDK.

--- lib/settings.sh
+++ lib/settings.sh
@@ -18,4 +18,4 @@ export MAC_OS_CONFIG_PATH="../mac_os-config"
 
 # Java
 export JAVA_LABEL="Java SE Development Kit"
-export JAVA_DOWNLOAD_URL="http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html"
+export JAVA_DOWNLOAD_URL="https://www.oracle.com/technetwork/java/javase/downloads/jdk10-downloads-4416644.html"
