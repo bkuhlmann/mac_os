c161493c326f8666c499e138eade4b6119247eba
Brooke Kuhlmann <brooke@alchemists.io>
2022-10-24 18:43:09 -0600

    Updated to macOS Ventura
    Necessary to be compatible with the latest version of the macOS
    operating system.

--- lib/settings.sh
+++ lib/settings.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 IFS=$'\n\t'
 
-export MAC_OS_BOOT_DISK_CREATOR="/Applications/Install macOS Monterey.app/Contents/Resources/createinstallmedia"
+export MAC_OS_BOOT_DISK_CREATOR="/Applications/Install macOS Ventura.app/Contents/Resources/createinstallmedia"
 export MAC_OS_BOOT_DISK_PATH="/Volumes/Untitled"
 export MAC_OS_WORK_PATH=/tmp/downloads
 export MAC_OS_CONFIG_PATH="../mac_os-config"
